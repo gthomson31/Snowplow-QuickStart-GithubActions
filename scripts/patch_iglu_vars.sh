7275e9195afcf9bd043e38f42a3402186974505c
Greg Thomson <greg.thomson@snowplowanalytics.com>
2022-05-09 11:52:34 +0100

    adding in job dependecie

--- scripts/patch_iglu_vars.sh
+++ scripts/patch_iglu_vars.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # -- Navigate to the Repo 
-cd quickstart-examples/terraform/aws && pwd
+cd ~/quickstart-examples/terraform/aws && pwd
 # -- Find and replace for enviroment variables
 sed -i  's|prefix = .*|prefix = "'"$PREFIX"'"|' iglu_server/default/terraform.tfvars
 sed -i  's|vpc_id            = .*|vpc_id = "'"${VPC_ID}"'"|' iglu_server/default/terraform.tfvars
